e78a47cdc924383f45cb308bb5383882ee31493f
thomaspoul1 <ps.poulain.thomas@gmail.com>
2023-09-28 14:01:23 +0200

    28/09

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,9 +1,9 @@
 def puissance(a, b):
     #if not (isinstance(a, int) and isinstance(b, int)):
-	if type(a) is int:
-		print("entier")
-	if type(b) is int:
-		print("entier")
+	if not type(a) is int:
+		raise Exception("entier")
+	if not type(b) is int:
+		raise Exception("entier")
 	else:	
 		print("pas entier")
 	return a**b
